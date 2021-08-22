44d0f5e3fd9ed6049ecfee9f176c0c0f73e942e3
Christian Avendano <cavendano@apside.cl>
2021-08-21 20:22:57 -0400

    se agrega ruta Auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Auth from '../views/Auth.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Auth',
+    component: Auth
   },
   {
     path: '/about',
